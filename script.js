dae3c1e48b3745645292817ba9aa06aff18c18f1
innakuk <kukharinna@gmail.com>
2022-03-16 12:45:46 +0200

    ДЗ 3. Делаем таски

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let admin;
+let name;
+name = 'Джон';
+admin = name;
+alert(admin);
+
+let ourPlanet = 'Земля';
+let currentUserName = 'Агнешка';
+
+let pcsOranges = '10';
+let kgOranges = 10;
+console.log(typeof pcsOranges);
+console.log(typeof kgOranges);
